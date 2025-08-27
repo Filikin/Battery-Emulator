efe31f8fa6883d39c48c1323e3747a96d2bb2357
Range Therapy <eamon@eandekelly.eu>
2025-08-27 13:28:53 +0100

    KC5  8.16  5-54 KC-Master battery default RT settings

--- Software/USER_SETTINGS.cpp
+++ Software/USER_SETTINGS.cpp
@@ -23,7 +23,7 @@ volatile CAN_Configuration can_config = {
 
 std::string ssid = WIFI_SSID;             // Set in USER_SECRETS.h
 std::string password = WIFI_PASSWORD;     // Set in USER_SECRETS.h
-const char* ssidAP = "KC1 RJXZS Battery Emulator";  // Maximum of 63 characters, also used for device name on web interface
+const char* ssidAP = "KC-Master RJXZS Battery Emulator";  // Maximum of 63 characters, also used for device name on web interface
 const char* passwordAP = AP_PASSWORD;     // Set in USER_SECRETS.h
 const uint8_t wifi_channel = 0;           // Set to 0 for automatic channel selection
 
